478020b22fddca3e7fb79cff7e1b29fe4a4641ee
Vangel <venonomer7@abv.bg>
2023-07-22 17:00:53 +0300

    adding register logic

--- recipes/src/app/register/register.component.ts
+++ recipes/src/app/register/register.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { NgForm } from '@angular/forms';
 
+interface User{
+  email: string,
+  password: string
+}
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-  
+  user: User = {
+    email: '',
+    password: ''
+  };
+  onSubmit(registerForm: NgForm){
+    if(registerForm.valid){
+      console.log(this.user);
+    }
+    else{
+      alert("Your form isn't valid");
+    }
+  }
+
 }
