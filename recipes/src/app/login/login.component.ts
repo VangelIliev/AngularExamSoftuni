89044cc5d68797aff67595d00f1f51506ced726e
Vangel <venonomer7@abv.bg>
2023-07-22 17:11:44 +0300

    Implementing forms logic for login

--- recipes/src/app/login/login.component.ts
+++ recipes/src/app/login/login.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
+interface User{
+  email: string,
+  password: string
+}
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
+  user: User = {
+    email: '',
+    password: ''
+  };
+  onSubmit(loginForm: NgForm){
+    if(loginForm.valid){
+      console.log(this.user);
+    }
+    else{
+      alert("Your form isn't valid");
+    }
+  }
 }
