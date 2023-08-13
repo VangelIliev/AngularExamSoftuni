e6cfee11fdbe9b7e38d01038c2540abfd48a9988
Vangel <venonomer7@abv.bg>
2023-08-13 09:50:12 +0300

    adding recipe service

--- recipes/src/app/add-recipe/add-recipe.component.ts
+++ recipes/src/app/add-recipe/add-recipe.component.ts
@@ -1,29 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, ViewChild  } from '@angular/core';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-add-recipe',
   templateUrl: './add-recipe.component.html',
   styleUrls: ['./add-recipe.component.scss']
 })
-export class AddRecipeComponent implements OnInit {
-  recipeForm!: FormGroup;
-
-  constructor(private formBuilder: FormBuilder) { }
-  ngOnInit() {
-    this.recipeForm = this.formBuilder.group({
-      title: ['', Validators.required],
-      description: ['', Validators.required],
-      // Add more form controls for other recipe details here
-    });
-  }
+export class AddRecipeComponent{
+  @ViewChild('addForm') addForm!: NgForm; 
   onSubmit() {
-    if (this.recipeForm.invalid) {
-      return;
-    }
-
     // Access the form values and add the recipe to your database or perform other actions.
-    const recipeData = this.recipeForm.value;
-    console.log(recipeData); // Replace this with your logic to save the recipe
+    var {recipeName, recipeImage, recipeCategory, recipePreparation, recipeServings, recipeDescription} = this.addForm.value;
+    console.log(recipeName); // Replace this with your logic to save the recipe
     // You can call a service to save the recipe data to your backend or use any other approach.
   }
 }
