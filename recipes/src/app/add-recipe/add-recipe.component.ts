b049db74530e62685082d305e9bdd2fd7238d494
Vangel <venonomer7@abv.bg>
2023-08-13 20:10:20 +0300

    Implement add recipe simple way

--- recipes/src/app/add-recipe/add-recipe.component.ts
+++ recipes/src/app/add-recipe/add-recipe.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ViewChild  } from '@angular/core';
+import { Component, ViewChild  } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { RecipeService } from '../services/recipe.service';
 @Component({
   selector: 'app-add-recipe',
   templateUrl: './add-recipe.component.html',
@@ -7,10 +8,34 @@ import { NgForm } from '@angular/forms';
 })
 export class AddRecipeComponent{
   @ViewChild('addForm') addForm!: NgForm; 
+  constructor(private recipeService: RecipeService){
+
+  }
   onSubmit() {
     // Access the form values and add the recipe to your database or perform other actions.
     var {recipeName, recipeImage, recipeCategory, recipePreparation, recipeServings, recipeDescription} = this.addForm.value;
-    console.log(recipeName); // Replace this with your logic to save the recipe
-    // You can call a service to save the recipe data to your backend or use any other approach.
+    var user = localStorage.getItem('user') ;
+    if(user != null){
+      const parsedObject = JSON.parse(user);
+      var uid = parsedObject.uid;
+      this.recipeService.addRecipe(
+      {
+        user:uid,
+        Name:recipeName,
+        Image:recipeImage,
+        Category:recipeCategory,
+        Preparation:recipePreparation,
+        Servings:recipeServings,
+        Description:recipeDescription
+      }).then(() => {
+        alert("Data has been added to Firestore");
+      })
+      .catch(error => {
+        alert('Error adding data to Firestore: ' + error);
+      });
+    }
+    else{
+      alert('user not found');
+    }
   }
 }
--- recipes/src/app/services/recipe.service.ts
+++ recipes/src/app/services/recipe.service.ts
@@ -1,6 +1,21 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore'; 
+import { Observable } from 'rxjs';
+@Injectable({
+    providedIn: 'root'
+})
 export class RecipeService{
+    constructor(private firestore: AngularFirestore) { }
 
-    async AddRecipe(){
-        
+    addRecipe(data: any): Promise<any> {
+        return this.firestore.collection('recipes').add(data);
+    }
+
+    getAllRecipes(): Observable<any[]> {
+        return this.firestore.collection('recipes').valueChanges();
+    }
+
+    getMyRecipes(): Observable<any[]>{
+        return this.firestore.collection('recipes').valueChanges();
     }
 }
